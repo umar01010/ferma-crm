658a36caa1d2dc75bd3da2c4bb1c09e69da45413
umar01010 <umarabdullaev304@gmail.com>
2024-08-25 14:43:06 -0400

    add new user structure

---
+++ src/assets/Pages/AddNewUser/AddNewUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./AddNewUser.css"
+
+const AddNewUser = () => {
+    return (
+        <section>
+            <div className="container">
+                add new user
+            </div>
+        </section>
+    )
+}
+
+export default AddNewUser
